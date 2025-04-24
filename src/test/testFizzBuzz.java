90aa5323f09eae604943fc37b880b1217779dbba
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amu.fr>
2025-04-24 14:25:51 +0200

    ajout de test complaimentaire

--- src/test/testFizzBuzz.java
+++ src/test/testFizzBuzz.java
@@ -12,6 +12,9 @@ public class testFizzBuzz {
         assertEquals("1", fizzBuzz.getResult(1));
         assertEquals("Fizz", fizzBuzz.getResult(3));
         assertEquals("Buzz", fizzBuzz.getResult(5));
+        assertEquals("Fizz", fizzBuzz.getResult(9));
+        assertEquals("Buzz", fizzBuzz.getResult(10));
         assertEquals("FizzBuzz", fizzBuzz.getResult(15));
+        assertEquals("FizzBuzz", fizzBuzz.getResult(0));
     }
 }
