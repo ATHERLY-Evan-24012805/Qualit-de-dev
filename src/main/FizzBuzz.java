f2b80e0f1b73fa5f26b791d057fd985856f3ae88
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amu.fr>
2025-04-24 13:59:40 +0200

    push de teste

---
+++ src/main/FizzBuzz.java
@@ -0,0 +1,15 @@
+package main;
+
+public class FizzBuzz {
+    public String getResult(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        else if (number % 5 == 0) {
+            return "Buzz";
+        }
+        else {
+            return String.valueOf(number);
+        }
+    }
+}
---
+++ src/test/testFizzBuzz.java
@@ -0,0 +1,7 @@
+package test;
+
+public class testFizzBuzz {
+    public static void main(String[] args) {
+
+    }
+}
